bf05df8ae3e14c37c5886b08fd523e7d5299864e
Kyle Thomas <Kyle.Thomas@atrium.com>
2016-01-27 08:42:40 -0600

    initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/cascadedd-dev'
+  },
+
+  // Seed database on startup
+  seedDB: true
+
+};
